f713a08356b4f5d16ce89f769b70254029ef71cb
matthijs.deheus <matthijs.heus@gmail.com>
2023-02-21 18:24:25 +0100

    Added parsing methods and GameData class

--- Redoublet-backend/Redoublet-backend/Controllers/BridgeGameLogic.cs
+++ Redoublet-backend/Redoublet-backend/Controllers/BridgeGameLogic.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Redoublet.Backend.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Redoublet_backend.Controllers
 {
@@ -14,10 +17,62 @@ namespace Redoublet_backend.Controllers
         }
 
         [HttpGet]
-        [Route("/GetCard")]
+        [Route("GetTestJson")]
         public string Get()
         {
-            return "Harten 4";
+            GameData gameData = new GameData();
+
+            Player[] players =
+            {
+                new Player()
+                {
+                    Name = "Pitcher",
+                    Cards = new List<Card>()
+                    {
+                        new Card()
+                        {
+                            Value = 3,
+                            Face = Face.Heart,
+                        }
+                    }
+                }
+            };
+
+            gameData.Players = players;
+            gameData.Dealer = Side.North;
+            gameData.Round = 3;
+            gameData.Trump = Face.Club;
+
+            return HelperFunctions.ParseGameData(gameData);
+        }
+
+        [HttpPost]
+        [Route("Test")]
+        public string StartGame(string jsonGameStateString)
+        {
+            GameData gameData = HelperFunctions.ParseJson(jsonGameStateString);
+
+            return gameData.Dealer.ToString();
+        }
+    }
+
+
+    public static class HelperFunctions
+    {
+        // Method to parse GameData object into json
+        public static GameData ParseJson(string jsonString)
+        {
+            GameData gameData = JsonSerializer.Deserialize<GameData>(jsonString);
+
+            return gameData;
+        }
+
+        // Method to parse json object into GameData
+        public static string ParseGameData(GameData gameData)
+        {
+            string json = JsonSerializer.Serialize(gameData);
+
+            return json;
         }
     }
 }
---
+++ Redoublet-backend/Redoublet-backend/Models/GameData.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Redoublet.Backend.Models
+{
+    public class GameData
+    {
+        public Player[] Players { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Side Dealer { get; set; }
+
+        public int Round { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Face Trump { get; set; }
+    }
+
+    public enum Side
+    {
+        North,
+        East,
+        South,
+        West,
+    }
+
+    public class Card
+    {
+        public int Value { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public Face Face { get; set; }
+    }
+
+    public enum Face
+    {
+        Diamond,
+        Heart,
+        Spade,
+        Club,
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public List<Card> Cards { get; set; }
+
+        public bool Vulnerable { get; set; }
+    }
+}
