5a7a18344c27e9d6fdffbcbb3529d1399fa27b88
Ruben Franquinet <ruben@franquinet.nl>
2023-02-21 17:47:03 +0100

    added login, lading and create account page

---
+++ Redoublet-website/src/components/LoginBox.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <form>
+      <p>Email address</p>
+      <input type="email">
+      <p>Password</p>
+      <input type="password">
+      <router-link to="home" custom v-slot="{ navigate }">
+        <button @click="navigate" role="link">Login</button>
+      </router-link>
+    </form>
+    <a>Forgot password</a>
+  </div>
+</template>
---
+++ Redoublet-website/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LoginBox from "../components/LoginBox.vue";
+</script>
+
+<template>
+    <router-link to="/" custom v-slot="{ navigate }">
+        <button @click="navigate" role="link">BACK</button>
+    </router-link>
+    <h1>Login</h1>
+    <LoginBox />
+</template>
