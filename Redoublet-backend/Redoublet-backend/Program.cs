411f4043b175d6ccb22d14ed2eeea284a8bb057b
matthijs.deheus <matthijs.heus@gmail.com>
2023-03-01 12:23:37 +0100

    expanded fetch

--- Redoublet-backend/Redoublet-backend/Program.cs
+++ Redoublet-backend/Redoublet-backend/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "policy",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:8080");
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("policy");
 
 app.UseAuthorization();
 
--- Redoublet-frontend/src/views/TestBoardView.vue
+++ Redoublet-frontend/src/views/TestBoardView.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 
 const startGame = async () => {
-    const response = await fetch('http://localhost:5000/api/StartGame?Name1=a&Name2=b&Name3=c&Name4=d&');
+    const response = await fetch('http://localhost:5000/api/StartGame', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            Name1: 'Speler 1',
+            Name2: 'Speler 2',
+            Name3: 'speler 3',
+            Name4: 'speler 4',
+        })
+
+    });
     const result = await response.json();
     console.log(result)
 }
@@ -11,9 +23,7 @@ const startGame = async () => {
 
 <template>
     <p>yo</p>
-    <!--
     <div>
         <button @click="startGame">Start</button>
     </div>
-    -->
 </template>
