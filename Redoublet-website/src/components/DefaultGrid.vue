2e16106b4514778ee58312287575fbc4f9600082
Ruben Franquinet <ruben@franquinet.nl>
2023-03-02 16:14:54 +0100

    Fixed Grid and add overview page

---
+++ Redoublet-website/src/components/DefaultGrid.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps({
+  data: Array,
+  columns: Array
+})
+</script>
+
+<template>
+    <table>
+      <thead>
+      <tr>
+        <th v-for="key in columns">
+          {{ key }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in data">
+        <td v-for="key in columns">
+          {{ row[key] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+table, th, td {
+  border: 1px solid;
+}
+</style>
--- Redoublet-website/src/views/MultiplayerTableJoinView.vue
+++ Redoublet-website/src/views/MultiplayerTableJoinView.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
-import JoinGameGrid from "../components/JoinGameGrid.vue";
+import JoinGameGrid from "../components/DefaultGrid.vue";
+
+const gridColumns = ['Table', 'Players', 'Partner system', 'Avg rating', 'Countdown']
+const gridData = [
+  { Table: '#1', Players: '2 / 4 ', 'Partner system': 'ACOL', 'Avg rating': '7.5', Countdown: '01:50m'},
+  { Table: '#2', Players: '1 / 4 ', 'Partner system': 'ACOL', 'Avg rating': '8.0', Countdown: '03:50m'},
+  { Table: '#3', Players: '3 / 4 ', 'Partner system': '2-1', 'Avg rating': '6.0', Countdown: '02:50m'},
+  { Table: '#4', Players: '3 / 4 ', 'Partner system': '5cards high', 'Avg rating': '8.5', Countdown: '01:45m'},
+  { Table: '#5', Players: '2 / 4 ', 'Partner system': '5cards high', 'Avg rating': '6.5', Countdown: '01:30m'},
+  { Table: '#6', Players: '1 / 4 ', 'Partner system': 'NAYC', 'Avg rating': '6.5', Countdown: '02:05m'},
+  { Table: '#7', Players: '3 / 4 ', 'Partner system': 'NAYC', 'Avg rating': '5.5', Countdown: '01:15m'},
+  { Table: '#8', Players: '3 / 4 ', 'Partner system': '2-1', 'Avg rating': '7.5', Countdown: '03:50m'},
+]
 </script>
 
 <template>
     <router-link to="/multiplayer-table" custom v-slot="{ navigate }">
         <button @click="navigate" role="link">BACK</button>
     </router-link>
-    <JoinGameGrid />
+    <JoinGameGrid 
+    :data="gridData"
+    :columns="gridColumns"/>
 </template>
