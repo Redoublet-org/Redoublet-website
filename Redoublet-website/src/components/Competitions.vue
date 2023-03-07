61bc495d0839cfed6b1c67d14e8ecf92daeafb26
sebas42069 <s.a.lehikoinen@students.uu.nl>
2023-03-07 11:32:39 +0100

    cleanup competitions
    uses composition now instead of option

--- Redoublet-website/src/components/Competitions.vue
+++ Redoublet-website/src/components/Competitions.vue
@@ -1,62 +1,56 @@
-<script>
+<script setup lang="ts">
+import {ref} from "vue"
 var week = new Date()
 var day = week.getDay()
-var conversion = (7-day) * 60 * 60 * 24 * 1000 //find amount of time between today's date and sunday in ms
+var conversion = (7 - day) * 60 * 60 * 24 * 1000 //find amount of time between today's date and sunday in ms
 var time = week.getTime()
-var v = time+conversion
+var v = time + conversion
 week.setTime(v)
-
 var now = new Date()
 
-export default {
-  data() {
-    return {
-      weekDate: week.getDate() + '/' + (week.getMonth()+1) + ' 23:59h',
-      today: now.getDate() + '/' + (now.getMonth()+1) + ' 23:59h'
-    }
-  }
-}
+const weekDate = week.getDate() + '/' + (week.getMonth() + 1) + ' 23:59h'
+const today = now.getDate() + '/' + (now.getMonth() + 1) + ' 23:59h'
 </script>
 
 <template>
   <div class="container">
     <div class="row">
 
-  <div class="col-5">
-    <h3>DAILY COMPETITION</h3>
-    <br>
-    <router-link to="/daily-competition" custom v-slot="{ navigate }">
-      <button  @click="navigate" role="link"><u>PLAY</u><br>(Deadline: {{today}})</button>
-    </router-link>
+      <div class="col-5">
+        <h3>DAILY COMPETITION</h3>
+        <br>
+        <router-link to="/daily-competition" custom v-slot="{ navigate }">
+          <button @click="navigate" role="link"><u>PLAY</u><br>(Deadline: {{ today }})</button>
+        </router-link>
+      </div>
+      <div class="col-5 offset-2">
+        <h3>WEEKLY COMPETITION</h3>
+        <br>
+        <router-link to="/weekly-competition" custom v-slot="{ navigate }">
+          <button @click="navigate" role="link"><u>PLAY</u><br>(Deadline: {{ weekDate }})</button>
+        </router-link>
+      </div>
+    </div>
   </div>
-  <div class="col-5 offset-2">
-    <h3>WEEKLY COMPETITION</h3>
-    <br>
-    <router-link to="/weekly-competition" custom v-slot="{ navigate }">
-      <button  @click="navigate" role="link"><u>PLAY</u><br>(Deadline: {{weekDate}})</button>
-    </router-link>
-  </div>
-</div>
-</div>
 </template>
 
 <style scoped>
 .container {
-    width: 60%;
-    left: 30%;
-    margin-top: 100px;
+  width: 60%;
+  left: 30%;
+  margin-top: 100px;
 }
 
-button{
-    width: 100%;
-    background-color: #1D9EFF;
-    border: 1px white solid;
-    color: white;
-    font-family: Arial, Helvetica, sans-serif;
-    font-weight: 600;
-    padding-top: 14px;
-    padding-bottom: 14px;
-    font-size: 20px;
+button {
+  width: 100%;
+  background-color: #1D9EFF;
+  border: 1px white solid;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 600;
+  padding-top: 14px;
+  padding-bottom: 14px;
+  font-size: 20px;
 }
 
 h3 {
